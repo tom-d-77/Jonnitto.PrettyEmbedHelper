696fd6a6d8e572e42f5f63539f47827a80cddae3
thomas.dingeldey <thomas.dingeldey@camao.one>
2025-10-24 18:14:12 +0200

    Getting the node from EventInterface

--- Classes/CatchUpHook/AssetDownloadCatchUpHook.php
+++ Classes/CatchUpHook/AssetDownloadCatchUpHook.php
@@ -2,13 +2,19 @@
 
 namespace Jonnitto\PrettyEmbedHelper\CatchUpHook;
 
+use Jonnitto\PrettyEmbedHelper\Service\MetadataService;
 use Neos\ContentRepository\Core\EventStore\EventInterface;
+use Neos\ContentRepository\Core\NodeType\NodeTypeManager;
 use Neos\ContentRepository\Core\Projection\CatchUpHook\CatchUpHookInterface;
+use Neos\ContentRepository\Core\Projection\ContentGraph\ContentGraphReadModelInterface;
+use Neos\ContentRepository\Core\Projection\ContentGraph\VisibilityConstraints;
+use Neos\ContentRepository\Core\SharedModel\ContentRepository\ContentRepositoryId;
 use Neos\ContentRepository\Core\Subscription\SubscriptionStatus;
 use Neos\ContentRepository\Core\Feature\NodeCreation\Event\NodeAggregateWithNodeWasCreated;
 use Neos\ContentRepository\Core\Feature\NodeModification\Event\NodePropertiesWereSet;
 use Neos\EventStore\Model\EventEnvelope;
 use Neos\Flow\Annotations as Flow;
+use Neos\Flow\Log\Utility\LogEnvironment;
 use Psr\Log\LoggerInterface;
 
 class AssetDownloadCatchUpHook implements CatchUpHookInterface
@@ -16,6 +22,14 @@ class AssetDownloadCatchUpHook implements CatchUpHookInterface
 
     #[Flow\Inject]
     protected LoggerInterface $logger;
+
+    public function __construct(
+        private readonly ContentRepositoryId $contentRepositoryId,
+        private readonly ContentGraphReadModelInterface $contentGraphReadModel,
+        private readonly NodeTypeManager $nodeTypeManager,
+        private MetadataService $metadataService
+    ) {
+    }
 
     /**
      * @inheritDoc
@@ -38,11 +52,21 @@ class AssetDownloadCatchUpHook implements CatchUpHookInterface
      */
     public function onAfterEvent(EventInterface $eventInstance, EventEnvelope $eventEnvelope): void
     {
-        if (
-            $eventInstance instanceof NodeAggregateWithNodeWasCreated ||
-            $eventInstance instanceof NodePropertiesWereSet
-        ) {
+        if ($eventInstance instanceof NodeAggregateWithNodeWasCreated) {
             $this->logger->info('NodeAggregateWithNodeWasCreated', ['event' => $eventInstance::class]);
+            // $this->metadataService->onNodeAdded();
+        }
+
+        if ($eventInstance instanceof NodePropertiesWereSet) {
+            $contentGraph = $this->contentGraphReadModel->getContentGraph($eventInstance->getWorkspaceName());
+            $node = $contentGraph->getSubgraph(
+                $eventInstance->originDimensionSpacePoint->toDimensionSpacePoint(),  // right dimension?
+                VisibilityConstraints::createEmpty()
+            )->findNodeById($eventInstance->nodeAggregateId);
+            $this->logger->info(
+                'NodePropertiesWereSet: ' . $node->nodeTypeName . ' ' . $node->aggregateId,
+                LogEnvironment::fromMethodName(__METHOD__));
+            // $this->metadataService->updateDataFromService($node);
         }
     }
 
--- Classes/CatchUpHook/AssetDownloadCatchUpHookFactory.php
+++ Classes/CatchUpHook/AssetDownloadCatchUpHookFactory.php
@@ -2,15 +2,27 @@
 
 namespace Jonnitto\PrettyEmbedHelper\CatchUpHook;
 
+use Jonnitto\PrettyEmbedHelper\Service\MetadataService;
 use Neos\ContentRepository\Core\Projection\CatchUpHook\CatchUpHookFactoryDependencies;
 use Neos\ContentRepository\Core\Projection\CatchUpHook\CatchUpHookFactoryInterface;
 use Neos\ContentRepository\Core\Projection\CatchUpHook\CatchUpHookInterface;
 
+
 class AssetDownloadCatchUpHookFactory implements CatchUpHookFactoryInterface
 {
+    public function __construct(
+        private MetadataService $metadataService
+    ) {
+
+    }
 
     public function build(CatchUpHookFactoryDependencies $dependencies): CatchUpHookInterface
     {
-        return new AssetDownloadCatchUpHook();
+        return new AssetDownloadCatchUpHook(
+            $dependencies->contentRepositoryId,
+            $dependencies->projectionState,
+            $dependencies->nodeTypeManager,
+            $this->metadataService
+        );
     }
 }
